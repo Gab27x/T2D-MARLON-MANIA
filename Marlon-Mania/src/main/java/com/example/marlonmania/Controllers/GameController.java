840d9deacdc8bac990cf0775292b6a4308dc1f79
Gabriel Escobar <gabrielernestoeb@outlook.com>
2023-11-21 08:47:54 -0500

    Game scene ui almost done

--- Marlon-Mania/src/main/java/com/example/marlonmania/Controllers/GameController.java
+++ Marlon-Mania/src/main/java/com/example/marlonmania/Controllers/GameController.java
@@ -40,57 +40,61 @@ public class GameController implements Initializable {
         displayGraph();
     }
 
-
     private void displayGraph() {
-        ArrayList<ListVertex<String>> vertices = newGame.getListGraph();
+    ArrayList<ListVertex<String>> vertices = newGame.getListGraph();
 
-        if (vertices.isEmpty()) {
-            System.out.println("La lista de nodos está vacía.");
-            return;
-        }
-
-        // Visualizar los nodos del grafo en el Group
-        for (ListVertex<String> vertex : vertices) {
-            double posX = vertex.getPosX() * 55;
-            double posY = vertex.getPosY() * 55;
-
-            Circle circle = new Circle(posX, posY, vertex.getRadius());
-            circle.setFill(Color.LIGHTBLUE);
-            graphGroup.getChildren().add(circle);
-            // Agregar texto dentro del círculo
-
-            Text text = new Text(vertex.getValue());
-
-            // Ajusta la posición del texto para que esté centrado en la mitad del círculo
-            text.setX(posX - text.getLayoutBounds().getWidth() / 2);
-            text.setY(posY + text.getLayoutBounds().getHeight() / 4);
-
-            graphGroup.getChildren().add(text);
-
-
-            // Dibujar líneas desde este nodo a sus nodos adyacentes
-            for (ListEdge<String> edge : vertex.getEdges()) {
-                double endX = edge.getRightVertex().getPosX() * 55;
-                double endY = edge.getRightVertex().getPosY() * 55;
-
-                // Calcular la dirección y la distancia desde el centro del círculo hasta el borde
-                double dirX = endX - posX;
-                double dirY = endY - posY;
-                double length = Math.sqrt(dirX * dirX + dirY * dirY);
-
-                // Calcular los puntos de inicio y fin en el borde del círculo
-                double startXAdjusted = posX + (dirX / length) * vertex.getRadius();
-                double startYAdjusted = posY + (dirY / length) * vertex.getRadius();
-                double endXAdjusted = endX - (dirX / length) * edge.getRightVertex().getRadius();
-                double endYAdjusted = endY - (dirY / length) * edge.getRightVertex().getRadius();
-
-                Line line = new Line(startXAdjusted, startYAdjusted, endXAdjusted, endYAdjusted);
-                line.setStrokeWidth(3.0);
-                graphGroup.getChildren().add(line);
-            }
-        }
+    if (vertices.isEmpty()) {
+        System.out.println("La lista de nodos está vacía.");
+        return;
     }
 
+    // Ajustes para la distribución de la cuadrícula
+    int gridSpacing = 55; // Espaciado entre nodos
+    int gridRows = 8;
+
+    // Visualizar los nodos del grafo en el Group
+    for (ListVertex<String> vertex : vertices) {
+        int row = vertex.getPosY();
+        int col = vertex.getPosX();
+
+        double posX = col * gridSpacing;
+        double posY = (gridRows - 1 - row) * gridSpacing; // Invertir las coordenadas y
+
+        Circle circle = new Circle(posX, posY, vertex.getRadius());
+        circle.setFill(Color.LIGHTBLUE);
+        graphGroup.getChildren().add(circle);
+
+        // Agregar texto dentro del círculo
+        Text text = new Text(vertex.getValue());
+        text.setX(posX - text.getLayoutBounds().getWidth() / 2);
+        text.setY(posY + text.getLayoutBounds().getHeight() / 4);
+        graphGroup.getChildren().add(text);
+
+        // Dibujar líneas desde este nodo a sus nodos adyacentes
+        for (ListEdge<String> edge : vertex.getEdges()) {
+            int endRow = edge.getRightVertex().getPosY();
+            int endCol = edge.getRightVertex().getPosX();
+
+            double endX = endCol * gridSpacing;
+            double endY = (gridRows - 1 - endRow) * gridSpacing; // Invertir las coordenadas y
+
+            // Calcular la dirección y la distancia desde el centro del círculo hasta el borde
+            double dirX = endX - posX;
+            double dirY = endY - posY;
+            double length = Math.sqrt(dirX * dirX + dirY * dirY);
+
+            // Calcular los puntos de inicio y fin en el borde del círculo
+            double startXAdjusted = posX + (dirX / length) * vertex.getRadius();
+            double startYAdjusted = posY + (dirY / length) * vertex.getRadius();
+            double endXAdjusted = endX - (dirX / length) * vertex.getRadius();
+            double endYAdjusted = endY - (dirY / length) * vertex.getRadius();
+
+            Line line = new Line(startXAdjusted, startYAdjusted, endXAdjusted, endYAdjusted);
+            line.setStrokeWidth(3.0);
+            graphGroup.getChildren().add(line);
+        }
+    }
+}
 
 
 }
